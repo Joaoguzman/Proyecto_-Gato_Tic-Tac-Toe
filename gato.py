489c89618c8327b0a93ae75a43835703547cea80
joaoguzman <joaogg@ug.uchile.cl>
2020-11-10 12:23:55 -0300

    actualizacion

--- gato.py
+++ gato.py
@@ -16,9 +16,9 @@ alternativas = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 
 def mostrar_tablero():
     print("\t", "1\t", "2\t", "3\t")
-    print("A", gato_tablero["A1"], "-  ", gato_tablero["A2"], "-  ", gato_tablero["A3"], "-  ")
-    print("B", gato_tablero["B1"], "-  ", gato_tablero["B2"], "-  ", gato_tablero["B3"], "-  ")
-    print("C", gato_tablero["C1"], "-  ", gato_tablero["C2"], "-  ", gato_tablero["C3"], "-  ")
+    print("A", gato_tablero["A1"], "-|  ", gato_tablero["A2"], "-|-  ", gato_tablero["A3"], "-|  ")
+    print("B", gato_tablero["B1"], "-|  ", gato_tablero["B2"], "-|-  ", gato_tablero["B3"], "-|  ")
+    print("C", gato_tablero["C1"], "-|  ", gato_tablero["C2"], "-|-  ", gato_tablero["C3"], "-|  ")
 
 mostrar_tablero()
 #Ingresa un valor. Si la casilla esta vacía lo asigna. Si no, indica que la posición está ocupada y continua con el ciclo while.
@@ -45,7 +45,28 @@ while True:
         continue
 mostrar_tablero()
 
+while True:
+    jugada2 = input("Ingrese su jugada")
+    if gato_tablero.get(jugada2) == "":
+        gato_tablero[jugada2] = "X"
+        break
+    else:
+        print("Posición ocupada")
+        continue
+#Se muestra el nuevo estado del tablero. 
+mostrar_tablero()
 
+#La CPU escoje un valor. 
+while True:
+    jugada_cpu_2 = random.choice(alternativas)
+    time.sleep(3)
+    if gato_tablero.get(jugada_cpu_2) == "":
+        gato_tablero[jugada_cpu_2] = "O"
+        break
+    else:
+        print("Posición ocupada")
+        continue
+mostrar_tablero()
 
 '''
 if gato_tablero.get(jugada1) == "":
